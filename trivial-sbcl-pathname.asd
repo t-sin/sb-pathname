59f8b953afc1a4d25b8d940693016c2429dc8521
t-sin <shinichi.tanaka45@gmail.com>
2025-02-16 19:16:32 +0900

    An escaping function for UNIX

---
+++ trivial-sbcl-pathname.asd
@@ -0,0 +1,6 @@
+(defsystem "trivial-sbcl-pathname"
+  :version "0.1.0"
+  :description "Utilities for SBCL's glob behaviors"
+  :author "t-sin <shinichi.tanaka45@gmail.com>"
+  :license "MIT"
+  :components ((:file "trivial-sbcl-pathname")))
---
+++ trivial-sbcl-pathname.lisp
@@ -0,0 +1,27 @@
+(defpackage :trivial-sbcl-pathname
+  (:nicknames :sb-path)
+  (:use :cl)
+  (:export :escape-glob))
+(in-package :trivial-sbcl-pathname)
+
+(defparameter +glob-chars+
+  '((#\? . #.(format nil "~a~a" #\\ #\?))
+    (#\* . #.(format nil "~a~a" #\\ #\*))
+    (#\[ . #.(format nil "~a~a" #\\ #\[))))
+
+(defun glob-char (ch)
+  (cdr (assoc ch +glob-chars+ :test #'char=)))
+
+(defun glob-char-p (ch)
+  (not (null (glob-char ch))))
+
+;; TODO: it's a temporal name.
+;; I don't like this name and if escaping behaviors are different on other platform, this name is not suitable.
+(defun escape-glob (str)
+  (with-output-to-string (stream)
+    (loop
+      :for ch :across str
+      :for escaped := (glob-char ch)
+      :do (if (null escaped)
+              (write-char ch stream)
+              (write-string escaped stream)))))
