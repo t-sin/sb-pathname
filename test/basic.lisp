e3577f66602c7c2266f54e7bfce8714cb2e54b50
t-sin <shinichi.tanaka45@gmail.com>
2025-02-17 21:36:56 +0900

    Fix some mistakes

--- test/basic.lisp
+++ test/basic.lisp
@@ -2,27 +2,26 @@
   (:use :cl :rove))
 (in-package :sb-pathname/test.basic)
 
-(defun length-of-escaped-is-2 (ch)
-  (let* ((escaped (sb-path::escape-char ch))
-         (len (length escaped)))
-    (ok (= len 2)
-        (format nil "a char ~s is espcaped as ~s, its length should be 2"
-                ch escaped))))
+(defun char-escaped-as (ch expected)
+  (let* ((escaped (sb-path::escape-char ch)))
+    (ok (string= escaped expected)
+        (format nil "a char ~s is escaped as ~s, but expected ~s"
+                ch escaped expected))))
 
 #+windows
-(deftest escaped-string-length/windows
-  (length-of-escaped-is-2 #\[))
+(deftest escape-char/windows
+  (char-escaped-as #\[ "^["))
 
 #-windows
-(deftest escaped-string-length/unix
-  (length-of-escaped-is-2 #\?)
-  (length-of-escaped-is-2 #\*)
-  (length-of-escaped-is-2 #\[))
+(deftest escape-char/unix
+  (char-escaped-as #\? "\\?")
+  (char-escaped-as #\* "\\*")
+  (char-escaped-as #\[ "\\["))
 
 (defun is-escaped-correctly (input expected)
   (let ((actual (sb-path:to-sb-pathname input)))
     (ok (string= actual expected)
-        (format nil "~s is escaped:~%~tactual: ~s~%~texpected: ~s"
+        (format nil "~s is escaped:~%~tactual:   ~s~%~texpected: ~s"
                 input expected actual))))
 
 #+windows
@@ -31,18 +30,16 @@
     (is-escaped-correctly "foobar2000.exe" "foobar2000.exe")
     (is-escaped-correctly "src/pages/articles/[id]/index.tsx" "src/pages/articles/^[id]/index.tsx"))
   (testing "not escaped caractors: '{'"
-    ;(is-escaped-correctly "{hoge,fuga,piyo}.jpg" "^{hoge,fuga,piyo}.jpg")))
     (is-escaped-correctly "{hoge,fuga,piyo}.jpg" "{hoge,fuga,piyo}.jpg")))
 
 #-windows
 (deftest to-sb-pathname/unix
   (testing "escaped charactors: '?', '*', '['"
     (is-escaped-correctly "foobar2000.exe" "foobar2000.exe")
-    (is-escaped-correctly "IMG_0042.jp*g" "IMG_0042.jp\\*g")
+    (is-escaped-correctly "IMG_0042.jp?g" "IMG_0042.jp\\?g")
     (is-escaped-correctly "*.jpg" "\\*.jpg")
     (is-escaped-correctly "src/pages/articles/[id]/index.tsx" "src/pages/articles/\\[id]/index.tsx"))
   (testing "not escaped caractors: '{'"
-    ;(is-escaped-correctly "{hoge,fuga,piyo}.jpg" "\\{hoge,fuga,piyo}.jpg")))
     (is-escaped-correctly "{hoge,fuga,piyo}.jpg" "{hoge,fuga,piyo}.jpg")))
 
 (defun is-unescaped (input expected)
@@ -89,8 +86,8 @@
   (testing "special characters on Windows"
     (identical-by-two-conversion "C:\\Program Files\\Abc Soft\\Abc.exe"))
   (testing "escaped on Windows"
-    (identical-by-two-conversion "src/pages/articles/^[id]/index.tsx"))
+    (identical-by-two-conversion "src/pages/articles/[id]/index.tsx"))
   (testing "escaped on UNIX"
-    (identical-by-two-conversion "IMG_0042.jp\\?g")
-    (identical-by-two-conversion "\\*.jpg")
-    (identical-by-two-conversion "src/pages/articles/\\[id]/index.tsx")))
+    (identical-by-two-conversion "IMG_0042.jp?g")
+    (identical-by-two-conversion "*.jpg")
+    (identical-by-two-conversion "src/pages/articles/[id]/index.tsx")))
