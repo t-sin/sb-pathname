fb91a6a5d4a0a5a8a9a6c60a68a7ce85af1b6805
t-sin <shinichi.tanaka45@gmail.com>
2025-02-17 16:25:18 +0900

    Add windows-spesific tests

--- test/basic.lisp
+++ test/basic.lisp
@@ -9,7 +9,12 @@
         (format nil "a char ~s is espcaped as ~s, its length should be 2"
                 ch escaped))))
 
-(deftest escaped-string-length
+#+windows
+(deftest escaped-string-length/windows
+  (length-of-escaped-is-2 #\[))
+
+#-windows
+(deftest escaped-string-length/unix
   (length-of-escaped-is-2 #\?)
   (length-of-escaped-is-2 #\*)
   (length-of-escaped-is-2 #\[))
@@ -20,7 +25,17 @@
         (format nil "~s is escaped:~%~tactual: ~s~%~texpected: ~s"
                 input expected actual))))
 
-(deftest to-sb-pathname
+#+windows
+(deftest to-sb-pathname/windows
+  (testing "escaped charactors: '['"
+    (is-escaped-correctly "foobar2000.exe" "foobar2000.exe")
+    (is-escaped-correctly "src/pages/articles/[id]/index.tsx" "src/pages/articles/^[id]/index.tsx"))
+  (testing "not escaped caractors: '{'"
+    ;(is-escaped-correctly "{hoge,fuga,piyo}.jpg" "^{hoge,fuga,piyo}.jpg")))
+    (is-escaped-correctly "{hoge,fuga,piyo}.jpg" "{hoge,fuga,piyo}.jpg")))
+
+#-windows
+(deftest to-sb-pathname/unix
   (testing "escaped charactors: '?', '*', '['"
     (is-escaped-correctly "foobar2000.exe" "foobar2000.exe")
     (is-escaped-correctly "IMG_0042.jp*g" "IMG_0042.jp\\*g")
